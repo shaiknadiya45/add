5c0e466996d4c396ab03c901ea6fac3a88004743
JNTUHCSE12 <JNTUHCSE12@JNTUHCSE12>
2019-04-02 17:23:10 +0530

    first project

---
+++ add/src/add/Add.java
@@ -0,0 +1,19 @@
+package add;
+
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		int a,b;
+		System.out.println("enter the values of a,b:");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int c=a+b;
+		System.out.println(c );
+	}
+
+}
